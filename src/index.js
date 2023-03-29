3cc8577bc9406d0d17c0eafefb8dcadad6064b3f
Mohieb <dev.mohieb@gmail.com>
2023-03-29 20:22:09 +0300

    finish most of the app
    will teak the mobile design later and will add light-dark mode button.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
-document.querySelector('form').addEventListener('submit', (e) => {
-    e.preventDefault()
-})
+const form = document.querySelector("form");
+const colorsSection = document.querySelector("#colors");
+const colorContainers = colorsSection.children;
+
+// first fetch to setup the page
+fetch(
+    `https://www.thecolorapi.com/scheme?hex=000000&mode=monochrome&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      // used a for loop to connect the data array and the
+      // divs array
+      for (let i = 0; i < colorContainers.length; i++) {
+        // colored divs
+        colorContainers[i].children[0].style.backgroundColor =
+          data.colors[i].hex.value;
+
+        colorContainers[i].children[0].setAttribute(
+          "data-value",
+          data.colors[i].hex.value
+        );
+        // color value divs
+        colorContainers[i].children[1].textContent = data.colors[i].hex.value;
+
+        colorContainers[i].children[1].setAttribute(
+          "data-value",
+          data.colors[i].hex.value
+        );
+      }
+    });
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const colorData = {
+    seedcolor: formData.get("seedcolor").slice(1),
+    scheme: formData.get("scheme").toLowerCase(),
+  };
+  const { seedcolor, scheme } = colorData;
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${seedcolor}&mode=${scheme}&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      // used a for loop to connect the data array and the
+      // divs array
+      for (let i = 0; i < colorContainers.length; i++) {
+        // colored divs
+        colorContainers[i].children[0].style.backgroundColor =
+          data.colors[i].hex.value;
+
+        colorContainers[i].children[0].setAttribute(
+          "data-value",
+          data.colors[i].hex.value
+        );
+        // color value divs
+        colorContainers[i].children[1].textContent = data.colors[i].hex.value;
+
+        colorContainers[i].children[1].setAttribute(
+          "data-value",
+          data.colors[i].hex.value
+        );
+      }
+    });
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.value) {
+    navigator.clipboard.writeText(e.target.dataset.value);
+    console.log(e.target.dataset.value);
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Inter': 'Inter, sans-serif'
+      }
+    },
   },
   plugins: [],
-  darkMode: 'class'
+  // darkMode: 'class'
+  // will later add a button
 }
